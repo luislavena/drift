12c971fdacef686fd98a34455b1d11c52c606c24
Luis Lavena <luislavena@gmail.com>
2022-01-28 19:18:37 +0100

    fixup! WIP Conductor

--- spec/drift/conductor_spec.cr
+++ spec/drift/conductor_spec.cr
@@ -1,17 +1,23 @@
 require "../spec_helper"
 require "sqlite3"
 
-def db_connection
+private def db_connection
   DB.connect "sqlite3:%3Amemory%3A"
 end
 
 describe Drift::Conductor do
   context "(folder with migrations)" do
-    pending "loads all migrations by order" do
+    it "loads all migrations in sequence" do
       conductor = Drift::Conductor.new
-      conductor.load_migrations fixture_path("sequence")
+
+      conductor.migrations.size.should eq(0)
+      conductor.load_migrations! fixture_path("sequence")
 
       conductor.migrations.size.should eq(2)
+      conductor.migrations.keys.should eq([
+        20211219152312_i64,
+        20211220182717_i64,
+      ])
     end
   end
 end
--- src/drift/conductor.cr
+++ src/drift/conductor.cr
@@ -3,7 +3,23 @@ require "./migration"
 
 module Drift
   class Conductor
-    def load_migrations(path : String | Path)
+    getter migrations : Hash(Int64, Drift::Migration)
+
+    def initialize
+      @migrations = Hash(Int64, Drift::Migration).new
+    end
+
+    def load_migrations!(path : String | Path)
+      # clear existing list of migrations
+      @migrations.clear
+
+      entries = Dir.glob(File.join(path, "*.sql")).sort!
+
+      entries.each do |entry|
+        migration = Migration.from_filename(entry)
+
+        @migrations[migration.id] = migration
+      end
     end
   end
 end
