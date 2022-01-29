2b8d8da98d1b395e4398d5615a0eec2ba9c7faac
Luis Lavena <luislavena@gmail.com>
2022-01-29 14:59:53 +0100

    WIP Conductor

--- spec/drift/conductor_spec.cr
+++ spec/drift/conductor_spec.cr
@@ -1,8 +1,60 @@
 require "../spec_helper"
 require "sqlite3"
 
+private def memory_db
+  DB.open "sqlite3:%3Amemory%3A"
+end
+
+private def loaded_conductor
+  conductor = Drift::Conductor.new
+  conductor.load_migrations! fixture_path("sequence")
+
+  conductor
+end
+
+private def populated_conductor
+  conductor = Drift::Conductor.new
+
+  sample_migrations.each do |migration|
+    conductor.add migration
+  end
+
+  conductor
+end
+
+private def quick_migration(id, filename)
+  Drift::Migration.new(id.to_i64, filename)
+end
+
+private def sample_migrations
+  [
+    quick_migration(1, "0001_create_users.sql"),
+    quick_migration(2, "0002_create_articles.sql"),
+    quick_migration(3, "0003_create_tags.sql"),
+    quick_migration(4, "0004_create_article_tag.sql"),
+  ]
+end
+
 describe Drift::Conductor do
-  context "(folder with migrations)" do
+  describe "#add" do
+    it "allows manually adding migrations to the list" do
+      conductor = Drift::Conductor.new
+
+      conductor.migrations.should be_empty
+
+      migration1 = quick_migration(1, "0001_first.sql")
+      migration2 = quick_migration(2, "0002_second.sql")
+      conductor.add migration1
+
+      conductor.migrations.size.should eq(1_i64)
+      conductor.migrations.keys.should eq([1_i64])
+
+      conductor.add migration2
+      conductor.migrations.keys.should eq([1_i64, 2_i64])
+    end
+  end
+
+  describe "#load_migrations!" do
     it "loads all migrations in sequence" do
       conductor = Drift::Conductor.new
 
@@ -13,6 +65,202 @@ describe Drift::Conductor do
       conductor.migrations.keys.should eq([
         20211219152312_i64,
         20211220182717_i64,
+      ])
+    end
+  end
+
+  describe "#applied?" do
+    it "returns false when queried non-applied migration id" do
+      db = memory_db
+      conductor = populated_conductor
+
+      conductor.applied?(db, 1_i64).should be_false
+    end
+  end
+
+  describe "#apply" do
+    it "applies the given migration once" do
+      db = memory_db
+      conductor = populated_conductor
+
+      conductor.applied?(db, 1_i64).should be_false
+
+      conductor.apply(db, 1_i64)
+      conductor.applied?(db, 1_i64).should be_true
+
+      conductor.apply(db, 1_i64)
+      conductor.applied?(db, 1_i64).should be_true
+    end
+
+    it "returns the applied batch" do
+      db = memory_db
+      conductor = populated_conductor
+
+      batch = conductor.apply(db, 1_i64)
+      batch.should eq(1_i64)
+
+      batch = conductor.apply(db, 2_i64)
+      batch.should eq(2_i64)
+    end
+
+    it "gives new batch number after rollback" do
+      db = memory_db
+      conductor = populated_conductor
+
+      batch_1 = conductor.apply(db, 1_i64)
+      batch_2 = conductor.apply(db, 2_i64)
+
+      conductor.rollback(db, 1_i64)
+
+      batch_3 = conductor.apply(db, 1_i64)
+      batch_3.should eq(3_i64)
+    end
+  end
+
+  describe "#apply_batch" do
+    it "applies the list of given migrations in a single batch" do
+      db = memory_db
+      conductor = populated_conductor
+
+      batch = conductor.apply_batch(db, [1_i64, 3_i64])
+      batch.should eq(1_i64)
+
+      conductor.applied?(db, 1_i64).should be_true
+      conductor.applied?(db, 2_i64).should be_false
+
+      batch = conductor.apply_batch(db, [2_i64, 4_i64])
+      batch.should eq(2_i64)
+
+      conductor.applied?(db, 2_i64).should be_true
+      conductor.applied?(db, 4_i64).should be_true
+    end
+
+    it "skips already applied migrations from the batch" do
+      db = memory_db
+      conductor = populated_conductor
+      conductor.apply(db, 1_i64)
+
+      batch = conductor.apply_batch(db, [1_i64, 2_i64, 3_i64])
+      batch.should eq(2_i64)
+      conductor.applied?(db, 1_i64).should be_true
+      conductor.applied?(db, 2_i64).should be_true
+      conductor.applied?(db, 3_i64).should be_true
+    end
+  end
+
+  describe "#apply_plan" do
+    it "returns a list of IDs of migrations to apply" do
+      db = memory_db
+      conductor = populated_conductor
+
+      plan = conductor.apply_plan(db)
+      plan.should eq([
+        1_i64,
+        2_i64,
+        3_i64,
+        4_i64,
+      ])
+    end
+
+    it "excludes already applied migrations from the list" do
+      db = memory_db
+      conductor = populated_conductor
+      conductor.apply(db, 3_i64)
+
+      plan = conductor.apply_plan(db)
+      plan.should eq([
+        1_i64,
+        2_i64,
+        4_i64,
+      ])
+    end
+  end
+
+  describe "#rollback" do
+    it "rolls back only applied migration" do
+      db = memory_db
+      conductor = populated_conductor
+
+      conductor.applied?(db, 1_i64).should be_false
+      conductor.rollback(db, 1_i64)
+
+      conductor.applied?(db, 1_i64).should be_false
+    end
+
+    it "rolls back applied migration" do
+      db = memory_db
+      conductor = populated_conductor
+
+      conductor.apply(db, 1_i64)
+      conductor.applied?(db, 1_i64).should be_true
+
+      conductor.rollback(db, 1_i64)
+      conductor.applied?(db, 1_i64).should be_false
+    end
+  end
+
+  describe "#rollback_batch_plan" do
+    it "returns nothing if batch does not exists" do
+      db = memory_db
+      conductor = populated_conductor
+
+      plan = conductor.rollback_batch_plan(db, 1_i64)
+      plan.should be_empty
+    end
+
+    it "returns only the list of IDs that needs to be rolled back" do
+      db = memory_db
+      conductor = populated_conductor
+
+      # batch 1
+      batch_1 = conductor.apply_batch(db, [1_i64, 4_i64])
+
+      # batch 2
+      batch_2 = conductor.apply_batch(db, [2_i64, 3_i64])
+
+      plan = conductor.rollback_batch_plan(db, batch_1)
+      plan.should eq([
+        4_i64,
+        1_i64,
+      ])
+    end
+  end
+
+  describe "#rollback_plan" do
+    it "returns nothing when no migration has to be rolled back" do
+      db = memory_db
+      conductor = populated_conductor
+
+      plan = conductor.rollback_plan(db)
+      plan.should be_empty
+    end
+
+    it "returns the list of IDs that needs to be rolled back" do
+      db = memory_db
+      conductor = populated_conductor
+
+      conductor.apply(db, 1_i64)
+      conductor.apply(db, 3_i64)
+
+      plan = conductor.rollback_plan(db)
+      plan.should eq([
+        3_i64,
+        1_i64,
+      ])
+    end
+
+    it "returns the list of IDs ordered to roll back ordered by batches" do
+      db = memory_db
+      conductor = populated_conductor
+      conductor.apply_batch(db, [1_i64, 4_i64])
+      conductor.apply_batch(db, [2_i64, 3_i64])
+
+      plan = conductor.rollback_plan(db)
+      plan.should eq([
+        3_i64,
+        2_i64,
+        4_i64,
+        1_i64,
       ])
     end
   end
--- src/drift/conductor.cr
+++ src/drift/conductor.cr
@@ -1,12 +1,51 @@
 require "db"
+require "uri"
+
 require "./migration"
 
 module Drift
   class Conductor
+    private MigrationRow = {id: Int64, batch: Int32, duration_ns: Int64, applied_at: Time}
+
     getter migrations : Hash(Int64, Drift::Migration)
 
     def initialize
       @migrations = Hash(Int64, Drift::Migration).new
+    end
+
+    def add(migration : Migration)
+      @migrations[migration.id] = migration
+    end
+
+    def applied?(db, id : Int64)
+      query_migration_applied?(db, id)
+    end
+
+    def apply(db, id : Int64)
+      apply_batch(db, [id])
+    end
+
+    def apply_batch(db, ids : Array(Int64))
+      applied_ids = query_applied_migrations(db).map(&.[:id])
+
+      batch = query_last_batch(db) + 1
+      final_ids = (ids - applied_ids)
+
+      final_ids.each do |id|
+        migration = @migrations[id]
+        duration = Time.measure { perform_up(db, migration) }
+
+        track_migration(db, id, batch, duration)
+      end
+
+      batch
+    end
+
+    def apply_plan(db)
+      available_ids = @migrations.keys.to_set
+      applied_ids = query_applied_migrations(db).map(&.[:id])
+
+      (available_ids - applied_ids).to_a.sort!
     end
 
     def load_migrations!(path : String | Path)
@@ -17,9 +56,102 @@ module Drift
 
       entries.each do |entry|
         migration = Migration.from_filename(entry)
-
-        @migrations[migration.id] = migration
+        add migration
       end
+    end
+
+    def rollback(db, id : Int64)
+      return unless query_migration_applied?(db, id)
+
+      migration = @migrations[id]
+
+      perform_down(db, migration)
+      untrack_migration(db, id)
+    end
+
+    def rollback_batch_plan(db, batch : Int64)
+      batch_migrations_ids = query_applied_migrations(db).select { |row|
+        row[:batch] == batch
+      }.map(&.[:id])
+
+      batch_migrations_ids.reverse!
+    end
+
+    def rollback_plan(db)
+      reversed_batch_ids = query_applied_migrations(db).sort_by! { |row|
+        {-row[:batch], -row[:id]}
+      }.map(&.[:id])
+
+      reversed_batch_ids
+    end
+
+    # FIXME: leaking internals of Drift::Migration
+    private def perform_down(db, migration)
+      statements = migration.statements_for(:down)
+      statements.each do |stmt|
+        db.exec stmt
+      end
+    end
+
+    # FIXME: leaking internals of Drift::Migration
+    private def perform_up(db, migration)
+      statements = migration.statements_for(:up)
+      statements.each do |stmt|
+        db.exec stmt
+      end
+    end
+
+    # TODO: extract and make modular for other adapters
+    private def verify_migration_table(db)
+      begin
+        # try verify migrations table exists
+        db.scalar("SELECT COUNT(id) FROM schema_migrations;").as(Int64)
+      rescue ex : Exception
+        # if not found, create it
+        db.exec <<-SQL
+          CREATE TABLE IF NOT EXISTS schema_migrations (
+              id INTEGER PRIMARY KEY NOT NULL,
+              batch INTEGER NOT NULL,
+              duration_ns INTEGER NOT NULL,
+              applied_at TEXT NOT NULL
+          );
+          SQL
+      end
+    end
+
+    private def query_applied_migrations(db)
+      verify_migration_table(db)
+
+      db.query_all("SELECT id, batch, duration_ns, applied_at FROM schema_migrations ORDER BY id ASC;", as: MigrationRow)
+    end
+
+    private def query_last_batch(db)
+      verify_migration_table(db)
+
+      db.scalar("SELECT COALESCE(MAX(batch), 0) FROM schema_migrations;").as(Int64)
+    end
+
+    private def query_migration_applied?(db, id : Int64)
+      verify_migration_table(db)
+
+      retrieved_id = db.query_one?("SELECT id FROM schema_migrations WHERE id = ?;", id, as: Int64)
+
+      retrieved_id == id
+    end
+
+    private def track_migration(db, id, batch, duration : Time::Span)
+      verify_migration_table(db)
+
+      duration_ns = duration.total_nanoseconds.to_i64
+      applied_at = Time.utc
+
+      db.exec "INSERT INTO schema_migrations (id, batch, duration_ns, applied_at) VALUES (?, ?, ?, ?);", id, batch, duration_ns, applied_at
+    end
+
+    private def untrack_migration(db, id)
+      verify_migration_table(db)
+
+      db.exec "DELETE FROM schema_migrations WHERE id = ?;", id
     end
   end
 end
