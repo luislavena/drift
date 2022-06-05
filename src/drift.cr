eb8a875bf70e2020db7a45f78f9f8e266ba4cbbe
Luis Lavena <luislavena@gmail.com>
2022-06-05 15:58:37 +0200

    Basic Drift::Migration implementation
    Allow manual creation and parsing from IO.

--- src/drift.cr
+++ src/drift.cr
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require "spec"
-require "../src/drift"
+module Drift
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+end
+
+require "./drift/migration"
