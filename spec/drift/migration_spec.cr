6d727d34e6e80a13bb90b6eccc5538dec0b76363
Luis Lavena <luislavena@gmail.com>
2022-06-05 15:58:37 +0200

    Basic Drift::Migration implementation
    Allow manual creation and parsing from IO.

---
+++ spec/drift/migration_spec.cr
@@ -0,0 +1,119 @@
+# Copyright 2022 Luis Lavena
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "../spec_helper"
+
+describe Drift::Migration do
+  describe ".new" do
+    it "accepts an optional filename" do
+      migration = Drift::Migration.new(1, "0001_create_users.sql")
+      migration.filename.should eq("0001_create_users.sql")
+
+      migration = Drift::Migration.new(2)
+      migration.filename.should be_nil
+    end
+
+    it "accepts multiple statements on each direction" do
+      migration = Drift::Migration.new(1)
+      migration.add(:up, "SELECT 1 AS one;")
+      migration.add(:up, "SELECT 2 AS two;")
+      migration.add(:down, "SELECT 3 AS three;")
+
+      migration.statements_for(:up).size.should eq(2)
+      migration.statements_for(:up).first.should eq("SELECT 1 AS one;")
+
+      migration.statements_for(:down).size.should eq(1)
+      migration.statements_for(:down).first.should eq("SELECT 3 AS three;")
+    end
+  end
+
+  describe ".from_io" do
+    it "accepts an optional filename" do
+      migration = Drift::Migration.from_io("", 1)
+      migration.id.should eq(1)
+      migration.filename.should be_nil
+
+      migration = Drift::Migration.from_io("", 2, "0001_create_users.sql")
+      migration.id.should eq(2)
+      migration.filename.should eq("0001_create_users.sql")
+    end
+
+    context "(magic comments)" do
+      it "parses statement on a given direction" do
+        data = <<-SQL
+          -- drift:up
+          SELECT 1;
+          SQL
+
+        migration = Drift::Migration.from_io(data, 1)
+        migration.statements_for(:up).size.should eq(1)
+        migration.statements_for(:up).first.should eq("SELECT 1;")
+      end
+
+      it "ignores statements without indicated direction" do
+        contents = <<-SQL
+          SELECT 4;
+          SQL
+
+        migration = Drift::Migration.from_io(contents, 1)
+        migration.statements_for(:up).should be_empty
+        migration.statements_for(:down).should be_empty
+      end
+
+      it "recognizes multiple statements in multiple directions" do
+        data = <<-SQL
+          -- drift:up
+          SELECT 1;
+          SELECT 2;
+
+          -- drift:down
+          SELECT 3;
+          SQL
+
+        migration = Drift::Migration.from_io(data, 1)
+        up_statements = migration.statements_for(:up)
+        up_statements.size.should eq(2)
+
+        down_statements = migration.statements_for(:down)
+        down_statements.size.should eq(1)
+        down_statements.first.should eq("SELECT 3;")
+      end
+
+      it "recognizes multi line statements" do
+        create_statement = <<-SQL
+          CREATE TABLE IF NOT EXISTS humans (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL
+          );
+          SQL
+
+        data = <<-SQL
+          -- drift:up
+          #{create_statement}
+          CREATE INDEX IF NOT EXISTS idx_humans_name ON humans(name);
+
+          -- drift:down
+          DROP TABLE IF EXISTS humans;
+          SQL
+
+        migration = Drift::Migration.from_io(data, 1)
+        migration.statements_for(:up).size.should eq(2)
+        migration.statements_for(:down).size.should eq(1)
+
+        create_statement = migration.statements_for(:up).first
+        create_statement.should eq(create_statement)
+      end
+    end
+  end
+end
---
+++ src/drift/migration.cr
@@ -0,0 +1,82 @@
+# Copyright 2022 Luis Lavena
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module Drift
+  class Migration
+    enum Direction
+      Up
+      Down
+    end
+
+    # :nodoc:
+    MAGIC_MARKER = "-- drift:"
+
+    getter id : Int64
+    getter filename : String? = nil
+
+    @statements : Hash(Direction, Array(String))
+
+    def initialize(@id : Int64, @filename : String? = nil)
+      @statements = Direction.values.to_h { |direction|
+        {direction, Array(String).new}
+      }
+    end
+
+    def add(direction : Direction, statement : String)
+      @statements[direction].push statement
+    end
+
+    def statements_for(direction : Direction)
+      @statements[direction]
+    end
+
+    def self.from_io(io, id : Int64, filename : String? = nil) : self
+      migration = new(id, filename)
+
+      buffer = IO::Memory.new
+      direction = nil
+
+      io.each_line do |line|
+        stripped_line = line.strip
+
+        # detect markers
+        if stripped_line.starts_with?(MAGIC_MARKER)
+          case stripped_line[MAGIC_MARKER.size..-1]
+          when "up"
+            direction = Direction::Up
+            next
+          when "down"
+            direction = Direction::Down
+            next
+          else
+            # TBD: support other commands?
+          end
+        end
+
+        next unless direction
+
+        # write raw line into buffer
+        buffer.puts line
+
+        if stripped_line.ends_with?(';')
+          # strip new line when saving the new statement
+          migration.add(direction, buffer.to_s.strip)
+          buffer.clear
+        end
+      end
+
+      migration
+    end
+  end
+end
